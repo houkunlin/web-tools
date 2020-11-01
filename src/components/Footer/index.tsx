9d5e69d61fc9b8ce2acc13c8d72b478449542d3d
侯坤林 <houkunlin@aliyun.com>
2020-11-01 13:20:00 +0800

    feat: 修改底部版权区域，增加 ICP 备案号和相关链接

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,27 +1,47 @@
 import React from 'react';
-import { GithubOutlined } from '@ant-design/icons';
+import { createFromIconfontCN, LinkOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-layout';
+import './index.less';
+
+const IconFont = createFromIconfontCN({
+  scriptUrl: [
+    "//at.alicdn.com/t/font_2171248_tekhmru6ev.js",
+    "/icon-icp.js",
+  ]
+})
 
 export default () => (
   <DefaultFooter
-    copyright="2019 蚂蚁金服体验技术部出品"
+    copyright="2020 十二后花园 版权所有"
     links={[
       {
-        key: 'Ant Design Pro',
-        title: 'Ant Design Pro',
-        href: 'https://pro.ant.design',
+        key: 'houkunlin.cn',
+        title: (<><LinkOutlined /> CN.</>),
+        href: 'http://houkunlin.cn',
         blankTarget: true,
       },
       {
-        key: 'github',
-        title: <GithubOutlined />,
-        href: 'https://github.com/ant-design/ant-design-pro',
+        key: 'houkunlin.com',
+        title: (<><LinkOutlined /> COM.</>),
+        href: 'http://houkunlin.com',
         blankTarget: true,
       },
       {
-        key: 'Ant Design',
-        title: 'Ant Design',
-        href: 'https://ant.design',
+        key: 'houkunlin.com.cn',
+        title: (<><LinkOutlined /> COM.CN.</>),
+        href: 'http://houkunlin.com.cn',
+        blankTarget: true,
+      },
+      {
+        key: '侯坤林.我爱你',
+        title: (<><LinkOutlined /> 我爱你.</>),
+        href: 'http://侯坤林.我爱你',
+        blankTarget: true,
+      },
+      {
+        key: '桂ICP备20000103号',
+        title: (<><IconFont type="icon-ICP-LOGO" /> 桂ICP备20000103号</>),
+        href: 'https://beian.miit.gov.cn',
         blankTarget: true,
       },
     ]}
