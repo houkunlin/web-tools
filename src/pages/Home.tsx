c97c106ce020b2df80116db6bfd59a023e040289
侯坤林 <houkunlin@aliyun.com>
2020-11-01 10:49:43 +0800

    feat: 首页 Home.tsx 增加 Json 数据格式化展示入口

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,11 +17,18 @@ class Home extends React.Component<ConnectProps, any> {
     return (
       <>
         <Typography.Title level={2}>工具列表</Typography.Title>
-        <Row>
-          <Col>
+        <Row gutter={10}>
+          <Col span={4}>
             <Link to="/utils/StompClient">
               <Card hoverable bordered>
                 <Card.Meta title="Stomp 客户端" description="在线 Stomp 客户端" />
+              </Card>
+            </Link>
+          </Col>
+          <Col span={4}>
+            <Link to="/utils/JsonView">
+              <Card hoverable bordered>
+                <Card.Meta title="Json 展示" description="在线 Json 格式化展示" />
               </Card>
             </Link>
           </Col>
