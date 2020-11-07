ae49eb03ec951e6ec64eb8e1098fd0a7ea4565a3
侯坤林 <houkunlin@aliyun.com>
2020-11-08 01:18:18 +0800

    style: 修改 config/config.ts 注释

--- config/config.ts
+++ config/config.ts
@@ -5,10 +5,11 @@ import proxy from './proxy';
 
 const { REACT_APP_ENV } = process.env;
 
+// @ts-ignore
 export default defineConfig({
   hash: true,
   history: {
-    type: 'hash',
+    type: 'browser',
   },
   copy: [
     {
@@ -114,8 +115,14 @@ export default defineConfig({
   // 使用 esbuild 作为压缩器。
   // esbuild: {},
   manifest: {
+    // 默认会使用 webpack 的 output.publicPath 配置
+    // publicPath: '/',
+    // 给所有文件路径加前缀
     basePath: '/',
   },
+  // https://umijs.org/zh-CN/config#publicpath
+  // 配置 webpack 的 publicPath。当打包的时候，webpack 会在静态文件路径前面添加 publicPath 的值，当你需要修改静态文件地址时，比如使用 CDN 部署，把 publicPath 的值设为 CDN 的值就可以。如果使用一些特殊的文件系统，比如混合开发或者 cordova 等技术，可以尝试将 publicPath 设置成 ./。
+  // publicPath: './',
   metas: [
     {
       name: 'keywords',
@@ -126,6 +133,7 @@ export default defineConfig({
       content: '侯坤林的个人工具箱',
     },
   ],
+  // 设置路由前缀，通常用于部署到非根目录。比如，你有路由 / 和 /users，然后设置了 base 为 /foo/，那么就可以通过 /foo/ 和 /foo/users 访问到之前的路由。
   // base: '.',
   // https://umijs.org/zh-CN/docs/deployment#html-%E5%90%8E%E7%BC%80
   // exportStatic: {
