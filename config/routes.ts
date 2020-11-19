f8b1437bd7606a80a75d5f53003831a7e9956934
侯坤林 <houkunlin@aliyun.com>
2020-11-19 11:31:20 +0800

    feat: 删除不需要的登陆页面和配置

--- config/routes.ts
+++ config/routes.ts
@@ -2,17 +2,6 @@
 
 export default [
   {
-    path: '/user',
-    layout: false,
-    routes: [
-      {
-        name: 'login',
-        path: '/user/login',
-        component: './user/login',
-      },
-    ],
-  },
-  {
     path: '/index',
     name: 'home',
     component: './Home',
