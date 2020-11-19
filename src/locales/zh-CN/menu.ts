df2d2f76380008a4965002fbabe87f60ec2b8c6d
侯坤林 <houkunlin@aliyun.com>
2020-11-19 11:34:44 +0800

    feat: 同步 Ant Design Pro 最新的 locales 配置信息

--- src/locales/zh-CN/menu.ts
+++ src/locales/zh-CN/menu.ts
@@ -49,5 +49,4 @@ export default {
   'menu.editor.flow': '流程编辑器',
   'menu.editor.mind': '脑图编辑器',
   'menu.editor.koni': '拓扑编辑器',
-  'menu.utils': '工具',
 };
--- src/pages/locales/zh-CN.ts
+++ src/pages/locales/zh-CN.ts
@@ -1,4 +1,5 @@
 export default {
+  'menu.utils': '工具',
   'menu.utils.StompClient': 'Stomp客户端',
   'menu.utils.JsonView': 'Json展示',
   'menu.utils.Highlight': '代码高亮',
